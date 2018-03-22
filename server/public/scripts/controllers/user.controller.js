2313c9cc8c1914103d820fb9ed76e9f75e6d957d
Caleb <calebmiskowiec94@gmail.com>
2018-03-22 15:24:58 -0500

    post works for bfast lunch and dinner with date

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('UserController', ['$http','UserService', function($http, UserService) {
+myApp.controller('UserController', ['$http', 'UserService', function ($http, UserService) {
   console.log('UserController created');
   var self = this;
   self.userService = UserService;
@@ -12,13 +12,51 @@ myApp.controller('UserController', ['$http','UserService', function($http, UserS
   self.saveMonday = function () {
 
     console.log('sending to server...', self.monday);
-    $http.post('/monday', self.monday).then(function (response) {
+    $http.post('/monday/b', self.monday).then(function (response) {
       console.log('successmonday');
 
     },
       function (response) {
         console.log('error');
-        self.message = "Something went wrong. Please try again."
+        self.message = "Something went wrong with your breakfast. Please try again."
+      });
+
+  }
+
+  self.mondaylunch = {
+    lfood: ''
+  };
+  self.message = '';
+
+  self.saveMondayLunch = function () {
+
+    console.log('sending to server...', self.mondaylunch);
+    $http.post('/monday/l', self.mondaylunch).then(function (response) {
+      console.log('successmondaylunch');
+
+    },
+      function (response) {
+        console.log('error');
+        self.message = "Something went wrong with your lunch. Please try again."
+      });
+
+  }
+  //object and function for dinner post
+  self.mondaydinner = {
+    dfood: ''
+  };
+  self.message = '';
+
+  self.saveMondayDinner = function () {
+
+    console.log('sending to server...', self.mondaydinner);
+    $http.post('/monday/d', self.mondaydinner).then(function (response) {
+      console.log('successmondaydinner');
+
+    },
+      function (response) {
+        console.log('error');
+        self.message = "Something went wrong with your dinner. Please try again."
       });
 
   }
--- server/routes/monday.router.js
+++ server/routes/monday.router.js
@@ -3,13 +3,8 @@ const encryptLib = require('../modules/encryption');
 const userStrategy = require('../strategies/sql.localstrategy');
 const pool = require('../modules/pool.js');
 const router = express.Router();
-
-
-
-// Handles POST request with new user data
-// The only thing different from this and every other post we've seen
-// is that the password gets encrypted before being inserted
-router.post('/', (req, res, next) => {
+//post for monday breakfast
+router.post('/b', (req, res, next) => {
     const bfood = req.body.bfood;
     
 
@@ -28,49 +23,46 @@ router.post('/', (req, res, next) => {
             }
         });
 });
-
-// Handles login form authenticate/login POST
-// userStrategy.authenticate('local') is middleware that we run on this route
-// this middleware will run our POST if successful
-// this middleware will send a 404 if not successful
-// router.post('/login', userStrategy.authenticate('local'), (req, res) => {
-//     res.sendStatus(200);
-// });
-// // clear all server session information about this user
-// router.get('/logout', (req, res) => {
-//     // Use passport's built-in method to log out the user
-//     req.logout();
-//     res.sendStatus(200);
-// });
+//post for monday lunch it reuses the local variable saveMonday{} but changes the property to lfood
+router.post('/l', (req, res, next) => {
+    const lfood = req.body.lfood;
 
 
+    var saveMonday = {
+        lfood: req.body.lfood
+
+    };
+    console.log('new lfoodmonday:', saveMonday);
+    pool.query('INSERT INTO monday (lfood) VALUES ($1)',
+        [saveMonday.lfood], (err, result) => {
+            if (err) {
+                console.log("Error inserting data: ", err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+            }
+        });
+});
+//post for monday dinner
+
+router.post('/d', (req, res, next) => {
+    const dfood = req.body.dfood;
 
 
+    var saveMonday = {
+        dfood: req.body.dfood
 
-
-//monday
-// router.post('/', (req, res, next) => {
-//     const monday_id = req.body.monday_id;
-//     const bFood = req.body.bFood;
-
-
-//     var saveFood = {
-//         monday_id: req.body.monday_id,
-//         bFood: req.body.bFood,
-//     }
-//     pool.query('INSERT INTO monday (monday_id, bfood) VALUES ($1, $2)',
-//         [saveFood.monday_id, saveFood.bfood], (err, result) => {
-//             if (err) {
-//                 console.log("Error inserting data: ", err);
-//                 res.sendStatus(500);
-//             } else {
-//                 res.sendStatus(201);
-//             }
-//         });
-//     console.log("var save savefood is", saveFood);
-//     res.sendStatus(200);
-
-// });
-
+    };
+    console.log('new dfoodmonday:', saveMonday);
+    pool.query('INSERT INTO monday (dfood) VALUES ($1)',
+        [saveMonday.dfood], (err, result) => {
+            if (err) {
+                console.log("Error inserting data: ", err);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+            }
+        });
+});
 
 module.exports = router;
